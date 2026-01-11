3e5ae67338f79276f4d03ff20724ce5851fd8053
Madhavan <ssmadhavan006@gmail.com>
2026-01-11 18:47:19 +0530

    Rectangular Star

---
+++ patterns/rectangular_star.java
@@ -0,0 +1,16 @@
+package patterns;
+
+class rectangular_star {
+    void pattern1(int n) {
+        for(int i=0; i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+    public static void main(String[] args){
+        rectangular_star obj = new rectangular_star();
+        obj.pattern1(5);
+    }
+}
