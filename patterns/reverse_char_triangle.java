599a513a9610961a8b91a3e22b63906f86b9056a
Madhavan <ssmadhavan006@gmail.com>
2026-01-14 18:32:18 +0530

    Reverse Character Triangle Pattern

---
+++ patterns/reverse_char_triangle.java
@@ -0,0 +1,19 @@
+package patterns;
+
+public class reverse_char_triangle {
+        public void pattern15(int n) {
+        for(int i=0;i<n;i++){
+            for(char ch ='A'; ch<='A'+(n-i-1);ch++){
+                System.out.print(ch+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args){
+        java.util.Scanner in = new java.util.Scanner(System.in);
+        int n = in.nextInt();
+        reverse_char_triangle s = new reverse_char_triangle();
+        s.pattern15(n);
+        in.close();
+    }
+}
