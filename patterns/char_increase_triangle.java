14886d58c20e6f189b7f6a3433c8346603b5b9a2
Madhavan <ssmadhavan006@gmail.com>
2026-01-14 12:32:38 +0530

    Character Increasing Triangle Pattern

---
+++ patterns/char_increase_triangle.java
@@ -0,0 +1,18 @@
+package patterns;
+import java.util.Scanner;
+public class char_increase_triangle {
+    public static void pattern14(int n) {
+        for(int i=1; i<=n; i++){
+            for(char ch='A'; ch<='A'+i;ch++){
+                System.out.print(ch+" ");
+            }
+            System.out.println();
+        }
+    }
+        public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        pattern14(n);
+        in.close();
+    }
+}
