c02c2ebce764d2eca663aed84cbd5b7747439362
Madhavan <ssmadhavan006@gmail.com>
2026-01-14 18:31:53 +0530

    Alpha Triangle Pattern

---
+++ patterns/alpha_triangle.java
@@ -0,0 +1,22 @@
+package patterns;
+
+public class alpha_triangle {
+        public void pattern18(int N) {
+        // Loop for each row
+        for (int i = 0; i < N; i++) {
+            // Print characters from ('A' + N - 1 - i) to ('A' + N - 1)
+            for (char ch = (char) ('A' + N - 1 - i); ch <= (char) ('A' + N - 1); ch++) {
+                System.out.print(ch + " ");
+            }
+            // Move to next line after each row
+            System.out.println();
+        }
+    }
+    public static void main(String[] args){
+        java.util.Scanner in = new java.util.Scanner(System.in);
+        int n = in.nextInt();
+        alpha_triangle s = new alpha_triangle();
+        s.pattern18(n);
+        in.close();
+    }
+}
