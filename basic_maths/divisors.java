358d8dd16faf10c832a3b05337a777de539b9322
Madhavan <ssmadhavan006@gmail.com>
2026-01-16 16:03:14 +0530

    Print all Divisors

---
+++ basic_maths/divisors.java
@@ -0,0 +1,21 @@
+package basic_maths;
+
+public class divisors {
+    public int[] divisors(int n) {
+        int[] divisors = new int[n];
+        int index = 0;
+        for(int i=1; i<=n; i++){
+            if(n%i==0){
+                System.out.print(i);
+            }
+        }
+        return divisors;
+    }
+    public static void main(String[] args) {
+        java.util.Scanner in = new java.util.Scanner(System.in);
+        int n = in.nextInt();
+        divisors div = new divisors();
+        div.divisors(n);
+        in.close();
+    }
+}
